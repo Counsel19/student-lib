9922d8e038077e8f03207f958f785442118b3bec
Okpabi Counsel <okpabicounsel@gmail.com>
2022-10-27 14:25:28 +0100

    client serving improved

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.resolve(__dirname, "./client/build")))
+app.use(express.static(path.resolve(__dirname, "./client/dist")))
 
 app.use(express.json());
 
@@ -41,7 +41,7 @@ app.use("/api/students/", authMiddleWare, studentsRouter);
 app.use("/api/departments/", departmentRouter);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
   });
   
 
